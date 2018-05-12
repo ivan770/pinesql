1ff19c81a9677f444076c1e2949567f2f18079ca
ivan770 <leshenko.ivan770@gmail.com>
2018-05-12 17:03:19 +0300

    Add query function

--- js/commands.js
+++ js/commands.js
@@ -15,3 +15,30 @@ function connect(host, user, password) {
   }
   });
 }
+
+function query(host, user, password, query) {
+  var con = mysql.createConnection({
+    host: `${host}`,
+    user: `${user}`,
+    password: `${password}`
+  });
+
+  con.connect(function(err) {
+    if (err){
+      appendLog(err)
+      appendLog("Check console for additional details (F12)")
+      throw err;
+    }else{
+    appendLog("Connected")
+  con.query(query, function (err, result) {
+    if (err){
+      appendLog(err)
+      appendLog("Check console for additional details (F12)")
+      throw err;
+    }else{
+      console.log("Result: " + result);
+    }
+      });
+    }
+  });
+}
