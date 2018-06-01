b4a5c7556c2859747a5127ee97978131b555b35f
ivan770 <leshenko.ivan770@gmail.com>
2018-06-01 23:25:46 +0300

    Update checker function

--- js/base.js
+++ js/base.js
@@ -3,3 +3,18 @@ function appendLog(text, prefix) {
     log.value = `<${prefix}> ${text}\n\n${currentText}`
     M.textareaAutoResize($('#log'))
 }
+
+function updateCheck(){
+  request({
+    url: 'https://api.github.com/repos/ivan770/pinesql/releases/latest',
+    headers: {
+      'User-Agent': 'pinesql-updater'
+    }
+  }, function (error, response, body) {
+        if (!error) {
+          if (compareVersions(`${pjson.version}`, `${JSON.parse(body).tag_name}`) == -1){
+            appendLog(`New version available: ${JSON.parse(body).tag_name}`, "UPDATE")
+          }
+        }
+      });
+}
