5919d7a46e68fe83ae8156496b80e8d0121a9016
ivan770 <leshenko.ivan770@gmail.com>
2018-06-03 15:00:59 +0300

    Replace multi-if

--- js/settings.js
+++ js/settings.js
@@ -1,27 +1,15 @@
-if(settings.has('settings_build') === true){
-  if(typeof settings.get('settings_build') === "boolean"){
-    if(settings.get('settings_build') === true){
+if(settings.has('settings_build') === true && typeof settings.get('settings_build') === "boolean" && settings.get('settings_build') === true){
       buildButton.style.display = "inline"
     } else {
       buildButton.style.display = "none"
     }
-  }
-}
 
-if(settings.has('settings_functiontime') === true){
-  if(typeof settings.get('settings_functiontime') === "boolean"){
-    if(settings.get('settings_functiontime') === true){
+if(settings.has('settings_functiontime') === true && typeof settings.get('settings_functiontime') === "boolean" && settings.get('settings_functiontime') === true){
       exectime.style.display = "inline"
     } else {
       exectime.style.display = "none"
     }
-  }
-}
 
-if(settings.has('settings_developer') === true){
-  if(typeof settings.get('settings_developer') === "boolean"){
-    if(settings.get('settings_developer') === true){
+if(settings.has('settings_developer') === true && typeof settings.get('settings_developer') === "boolean" && settings.get('settings_developer') === true){
       ipc.send('developer')
     }
-  }
-}
